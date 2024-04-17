007fce7986ef348d51555faa92901c49b5770573
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-17 15:54:42 -0400

    Added: supabase connection, drizzle-orm setup, and migrations scripts

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,25 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as dotenv from "dotenv";
+import * as schema from "../../../migrations/schema";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+dotenv.config({ path: ".env" });
+
+if (!process.env.DATABASE_URL) {
+  console.error("Cannot Find Database URL");
+}
+
+const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
+const db = drizzle(client, { schema });
+const migrateDB = async () => {
+  try {
+    console.log("Migrating Client");
+    await migrate(db, { migrationsFolder: "migrations" });
+    console.log("Successfully Migrated Client");
+  } catch (error: any) {
+    console.error("Error Migrating Client");
+    console.error(error.message);
+  }
+};
+migrateDB();
+export default db;
